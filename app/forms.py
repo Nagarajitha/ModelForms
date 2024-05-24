c19f979dbe9d5fa8ab8126328aa4d4801f68911e
Nagaranjitha Adhikari <nagaranjithareddy421@gmail.com>
2024-05-24 16:12:42 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -10,7 +10,12 @@ class TopicForm(forms.ModelForm):
 class WebpageForm(forms.ModelForm):
     class Meta:
         model = Webpage
-        fields = '__all__'
+        #fields = '__all__'  
+        #inorder to inlcude specific fields we use list of columns we need to include or we can go for exclude
+        fields = ['topic_name','name','name','url'] # same as exclude = ['url']
+        help_texts ={'topic_name':'should not be integrs','name':'only Alphabets'}
+        labels = {'topic_name':'Topic','name':'Name'} # label for input elements
+        widgets = {'url':forms.PasswordInput,'name':forms.Textarea}
 
 class AccessRecordForm(forms.ModelForm):
     class Meta:
